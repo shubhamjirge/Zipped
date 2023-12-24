edfea7f77f9fbc094c3d41ebfa77824c73cbab50
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2023-12-24 09:34:48 +0530

    Add files via upload

---
+++ Zipped.py
@@ -0,0 +1,13 @@
+"""
+Title     : Zipped!
+Subdomain : Built-Ins
+Domain    : Python
+Problem   : https://www.hackerrank.com/challenges/zipped/problem
+"""
+
+N, X = map(int, input().split())
+scores = []
+for _ in range(X):
+    scores.append(list(map(float, input().split())))
+for i in zip(*scores):
+    print(sum(i) / len(i))
